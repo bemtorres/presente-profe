159867d78c36041f464bbdcee8090cfd33239e83
profe benja <bej.mora@profesor.duoc.cl>
2023-09-21 03:54:40 -0300

    update

--- resources/views/planes/id/participantes/pdf.blade.php
+++ resources/views/planes/id/participantes/pdf.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 @push('css')
 
-{{-- <link href="{{ asset('vendors/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet"> --}}
+<style>
+.list-group-item-selected {
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+#lista {
+  display: none;
+}
 
+.cursor {
+  cursor: pointer;
+}
+</style>
 @endpush
 @section('content')
 @component('components.button._back')
@@ -16,26 +28,50 @@
     <div class="card shadow mb-4">
       <div class="card-body">
         <div class="row">
-          <div class="col-lg-12 mb-4">
-            <div class="card mb-4">
+          <div class="col-lg-4 mb-4">
+            <div class="card">
               <div class="card-body">
-                <div class="col-md-12 mb-3">
-                  <div class="d-flex align-items-center">
-                    <div class="avatar avatar-md">
-                      <img class="avatar-img" src="{{ $u->getImg() }}" alt="">
+                <ul class="list-group cursor">
+                  <li class="list-group-item d-flex justify-content-between align-items-center" id="mostrarLista">
+                    <div class="d-flex align-items-center">
+                      <div class="avatar avatar-md">
+                        <img class="avatar-img" src="{{ $u->getImg() }}" alt="">
+                      </div>
+                      <div class="ms-2">
+                        <span class="h6 mt-2 mt-sm-0">{{ $u->nombre_completo() }}</span>
+                        <p class="small m-0">{{ $u->correo }}</p>
+                      </div>
                     </div>
-                    <div class="ms-2">
-                      <span class="h6 mt-2 mt-sm-0">{{ $u->nombre_completo() }}</span>
-                      <p class="small m-0">{{ $u->correo }}</p>
+                    <div class="text-end">
+                      <i class="fa fa-chevron-down"></i>
                     </div>
-                  </div>
-                </div>
-                <a href="{{ route('pdf.diponibilidad', [$plan->id, $asociado->id]) }}" class="btn btn-danger">VER PDF</a>
+                  </li>
+                </ul>
+                <ul class="list-group" id="lista" style="display: none;">
+                  @foreach ($plan->asociado_plan as $a)
+                    @continue($a->id_usuario == $u->id)
+                    <li class="list-group-item list-group-item-action d-flex cursor" onclick="window.location = '{{ route('planes.participantes.showPDF',[$plan->id, $a->id]) }}'">
+                      <div class="d-flex align-items-center">
+                        <div class="avatar avatar-md">
+                          <img class="avatar-img" src="{{ $a->usuario->getImg() }}" alt="">
+                        </div>
+                        <div class="ms-2">
+                          <span class="h6 mt-2 mt-sm-0">{{ $a->usuario->nombre_completo() }}</span>
+                          <p class="small m-0">{{ $a->usuario->correo }}</p>
+                        </div>
+                      </div>
+                    </li>
+                  @endforeach
+                </ul>
               </div>
             </div>
           </div>
+
         </div>
-        <div class="row">
+        <div class="row justify-content-center">
+          <div class="col-md-12 text-center mb-3">
+            <a href="{{ route('pdf.diponibilidad', [$plan->id, $asociado->id]) }}" class="btn btn-danger btn-sm text-white"><i class="fa fa-file-pdf me-2"></i><strong>VER PDF</strong></a>
+          </div>
           <div class="col">
             <div class="card">
               <div class="card-body">
@@ -50,5 +86,22 @@
 </div>
 @endsection
 @push('js')
+<script>
+  $(document).ready(function () {
+    // Manejar el clic en el botón "Mostrar Lista"
+    $("#mostrarLista").click(function () {
+      $("#lista").slideToggle();
+    });
+
+    // Manejar la selección de elementos de la lista
+    $("#lista li").click(function () {
+      // Desmarcar todos los elementos
+      $("#lista li").removeClass("list-group-item-selected");
+
+      // Marcar el elemento seleccionado
+      $(this).addClass("list-group-item-selected");
+    });
+  });
+  </script>
 
 @endpush
