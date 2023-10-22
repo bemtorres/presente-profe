f848ca8ddfea2815c2f41c4ebd3cabd434d1bf49
profe benja <bej.mora@profesor.duoc.cl>
2023-10-21 21:49:28 -0300

    borrar th

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -38,7 +38,6 @@ class AuthController extends Controller
         return back()->with('info','Error. Intente nuevamente.');
       }
     } catch (\Throwable $th) {
-      return $th;
       return back()->with('info','Error. Intente nuevamente.');
     }
   }
